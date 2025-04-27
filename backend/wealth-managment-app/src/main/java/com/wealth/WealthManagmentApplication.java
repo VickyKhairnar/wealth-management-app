0ada62154b739d5afafcfd17cdc7fd5b9fa379a7
VickyKhairnar <khairnar375@gmail.com>
2025-04-28 01:17:20 +0530

    backend

---
+++ backend/wealth-managment-app/src/main/java/com/wealth/WealthManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.wealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WealthManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WealthManagmentApplication.class, args);
+	}
+
+}
